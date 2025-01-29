cafdcb89d10c772f9180348997ee8e2310dc8eea
Fizzzn <btordz6@gmail.com>
2025-01-29 21:02:05 +0000

    Done and done

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+public class Input{
 
+ public static void main(String [] args){
+    Scanner scanner = new Scanner(System.in);
+    //info gathering begin! 
+    String username;
+    int userage;
+    double weight;
+    boolean smokeryuck;
+    System.out.print("Hello, name please?:");
+    username = scanner.nextLine();
+    System.out.print("age: ");
+    userage = scanner.nextInt();
+    System.out.print("your weight[Ib]: ");
+    weight = scanner.nextDouble();
+    System.out.print("do you smoke?[T/F]: ");
+    smokeryuck = scanner.nextBoolean();
+    System.out.print("///////////////////////////////");
+    System.out.println("\nInformation summery:");
+    System.out.println("Name:" + username + "\nage:" + userage + "\nweight:" + weight + "\nSmoking status:" + smokeryuck);
+    
+    }
+
+
+
+}
